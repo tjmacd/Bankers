76766e9af6d4aa8ceb09d3952bbeb60d6240c9bb
nephna <ilunamist@gmail.com>
2015-11-20 12:13:37 -0500

    Initial stages of lab

--- banker.c
+++ banker.c
@@ -1,7 +1,7 @@
 /*
  * Banker's Algorithm for SOFE 3950U / CSCI 3020U: Operating Systems
  *
- * Copyright (C) 2015, <GROUP MEMBERS>
+ * Copyright (C) 2015, Muhammad Adham, Timothy MacDougall, Devin Westbye
  * All rights reserved.
  * 
  */
@@ -17,10 +17,11 @@
 #define NUM_CUSTOMERS 5
 #define NUM_RESOURCES 3
 
-
 // Put global environment variables here
 // Available amount of each resource
 int available[NUM_RESOURCES];
+
+int max[NUM_RESOURCES];
 
 // Maximum demand of each customer
 int maximum[NUM_CUSTOMERS][NUM_RESOURCES];
@@ -31,27 +32,54 @@ int allocation[NUM_CUSTOMERS][NUM_RESOURCES];
 // Remaining need of each customer
 int need[NUM_CUSTOMERS][NUM_RESOURCES];
 
+void* requestReleaseRepeat (void* arg) {
+    int customerNumber = (int) arg;
 
-// Define functions declared in banker.h here
-// bool request_res(int n_customer, int request[])
-// {
-//      ...
-// }
+    for (int i = 0; i < 3; i++) {
+        int randAmount[NUM_RESOURCES];
 
-// Release resources, returns true if successful
-// bool release_res(int n_customer, int release[])
-// {
-//      ...
-// }
+        for (int i = 0; i < NUM_RESOURCES; i++) {
+            randAmount[i] = rand() % max[i];
+            printf("%d\n", randAmount[i]);
+        }
+
+        //requestRes(customerNumber, randAmount);
+        //releaseRes(customerNumber, randAmount);
+    }
+    return NULL;
+}
+
+bool requestRes (int customerNumber, int amount[]) {
+    return false;
+}
+
+bool releaseRes (int customerNumber, int amount[]) {
+    return false;
+}
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    // define local variables
+    pthread_t customerZero, customerOne, customerTwo, customerThree, customerFour;
+
+    if (argc != NUM_RESOURCES+1) {
+        printf("Error: Incorrect amount of arguments supplied. Requires: %d Received: %d\n", NUM_RESOURCES, argc-1);
+        exit(0);
+    }
+
+    // initialize available resources
+    for (int i = 0; i < NUM_RESOURCES; i++) {
+        available[i] = atoi(argv[i+1]);
+        max[i] = atoi(argv[i+1]);
+    }
+
+    pthread_create(&customerZero, 0, requestReleaseRepeat, (void*) 0);
+    pthread_create(&customerOne, 0, requestReleaseRepeat, (void*) 1);
+    pthread_create(&customerTwo, 0, requestReleaseRepeat, (void*) 2);
+    pthread_create(&customerThree, 0, requestReleaseRepeat, (void*) 3);
+    pthread_create(&customerFour, 0, requestReleaseRepeat, (void*) 4);
+
     // ==================== YOUR CODE HERE ==================== //
-
-    // Read in arguments from CLI, NUM_RESOURCES is the number of arguments   
-    
-    // Allocate the available resources
 
     // Initialize the pthreads, locks, mutexes, etc.
 
@@ -64,6 +92,12 @@ int main(int argc, char *argv[])
     
     // If you are having issues try and limit the number of threads (NUM_CUSTOMERS)
     // to just 2 and focus on getting the multithreading working for just two threads
+
+    pthread_join(customerZero, NULL);
+    pthread_join(customerOne, NULL);
+    pthread_join(customerTwo, NULL);
+    pthread_join(customerThree, NULL);
+    pthread_join(customerFour, NULL);
 
     return EXIT_SUCCESS;
 }
